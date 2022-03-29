7a6064a40054c616425b8f853e2d9abab5da30eb
Steve Alex <stevealexrs@protonmail.com>
2022-03-29 18:23:34 +0100

    Set SolidityTypedInvocation method visibility to public

--- Sources/Web3ContractABI/ContractABI/Contract/SolidityTypedInvocation.swift
+++ Sources/Web3ContractABI/ContractABI/Contract/SolidityTypedInvocation.swift
@@ -21,7 +21,7 @@ public struct SolidityTypedInvocation<T: Decodable> {
     }
     
     /// Read data from the blockchain. Only available for constant functions.
-    func call(block: EthereumQuantityTag = .latest) async -> (T?, Error?) {
+    public func call(block: EthereumQuantityTag = .latest) async -> (T?, Error?) {
         let (dictionary, error) = await invocation.call(block: block)
         if let dictionary = dictionary, error == nil {
             do {
@@ -35,22 +35,22 @@ public struct SolidityTypedInvocation<T: Decodable> {
     }
     
     /// Generates an EthereumCall object
-    func createCall() -> EthereumCall? {
+    public func createCall() -> EthereumCall? {
         return self.invocation.createCall()
     }
     
     /// Generates an EthereumTransaction object
-    func createTransaction(nonce: EthereumQuantity?, from: EthereumAddress, value: EthereumQuantity?, gas: EthereumQuantity, gasPrice: EthereumQuantity?) -> EthereumTransaction? {
+    public func createTransaction(nonce: EthereumQuantity?, from: EthereumAddress, value: EthereumQuantity?, gas: EthereumQuantity, gasPrice: EthereumQuantity?) -> EthereumTransaction? {
         return self.invocation.createTransaction(nonce: nonce, from: from, value: value, gas: gas, gasPrice: gasPrice)
     }
     
     /// Write data to the blockchain. Only available for non-constant functions.
-    func send(nonce: EthereumQuantity?, from: EthereumAddress, value: EthereumQuantity?, gas: EthereumQuantity, gasPrice: EthereumQuantity?) async -> (EthereumData?, Error?) {
+    public func send(nonce: EthereumQuantity?, from: EthereumAddress, value: EthereumQuantity?, gas: EthereumQuantity, gasPrice: EthereumQuantity?) async -> (EthereumData?, Error?) {
         return await self.invocation.send(nonce: nonce, from: from, value: value, gas: gas, gasPrice: gasPrice)
     }
     
     /// Estimate how much gas is needed to execute this transaction.
-    func estimateGas(from: EthereumAddress?, gas: EthereumQuantity?, value: EthereumQuantity?) async -> (EthereumQuantity?, Error?) {
+    public func estimateGas(from: EthereumAddress?, gas: EthereumQuantity?, value: EthereumQuantity?) async -> (EthereumQuantity?, Error?) {
         return await self.invocation.estimateGas(from: from, gas: gas, value: value)
     }
 }
